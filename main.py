5868583f87f7f57835d3eb42cba7af01bf454769
Joey Copeland <josecope@iu.edu>
2019-10-04 14:36:26 -0400

    Finds sprites.

---
+++ main.py
@@ -0,0 +1,111 @@
+import sys, logging, open_color, arcade
+
+#check to make sure we are running the right version of Python
+version = (3,7)
+assert sys.version_info >= version, "This script requires at least Python {0}.{1}".format(version[0],version[1])
+
+#turn on logging, in case we have to leave ourselves debugging messages
+logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Space Shooter!"
+
+NUM_ENEMIES = 5
+STARTING_LOCATION = (400,100)
+BULLET_DAMAGE = 10
+ENEMY_HP = 100
+HIT_SCORE = 10
+KILL_SCORE = 100
+
+class Bullet(arcade.Sprite):
+    def __init__(self, position, velocity, damage):
+        super().__init__("assets/bullet.png", 0.5)
+        (self.center_x, self.center_y) = position
+        (self.dx, self.dy) = velocity
+        self.damage = damage
+
+    def update(self):
+        self.center_x += self.dx
+        self.center_y += self.dy
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__("assets/spaceship.png", 0.5)
+        (self.center_x, self.center_y) = STARTING_LOCATION
+
+class Enemy(arcade.Sprite):
+    def __init__(self, position):
+        super().__init__("assets/alien.png", 0.5)
+        self.hp = ENEMY_HP
+        (self.center_x, self.center_y) = position
+
+
+class Window(arcade.Window):
+
+    def __init__(self, width, height, title):
+
+        # Call the parent class's init function
+        super().__init__(width, height, title)
+
+        # Make the mouse disappear when it is over the window.
+        # So we just see our object, not the pointer.
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(open_color.black)
+
+
+
+    def setup(self):
+        pass 
+
+    def update(self, delta_time):
+        pass
+
+    def on_draw(self):
+        """ Called whenever we need to draw the window. """
+        arcade.start_render()
+
+
+
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """ Called to update our objects. Happens approximately 60 times per second."""
+        pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """ Called whenever the user presses a key. """
+        if key == arcade.key.LEFT:
+            print("Left")
+        elif key == arcade.key.RIGHT:
+            print("Right")
+        elif key == arcade.key.UP:
+            print("Up")
+        elif key == arcade.key.DOWN:
+            print("Down")
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        pass
+
+
+def main():
+    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
